529f8ab1b72c358ecf06d8b1222b31f195eabd48
tectiv3 <git@getpanda.me>
2020-12-03 13:54:29 +0900

    Add complete state to booking

--- src/Model/Booking.php
+++ src/Model/Booking.php
@@ -9,12 +9,14 @@ namespace MoovOne\TimekitPhpSdk\Model;
 class Booking
 {
     public const STATE_CONFIRM = 'confirm';
+    public const STATE_COMPLETE = 'complete';
     public const STATE_DECLINE = 'decline';
     public const STATE_CANCEL = 'cancel';
     public const STATE_CANCEL_BY_CUSTOMER = 'cancel_by_customer';
 
     public const AVAILABLE_STATES = [
         self::STATE_CONFIRM,
+        self::STATE_COMPLETE,
         self::STATE_DECLINE,
         self::STATE_CANCEL,
         self::STATE_CANCEL_BY_CUSTOMER,
